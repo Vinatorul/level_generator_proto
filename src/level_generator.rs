ecada7d03976b7371f1873b1dbe6de7d59f48323
Alexander <alexander@kuvaev.me>
2016-02-22 20:26:02 +0300

    corridors redesign

--- src/level_generator.rs
+++ src/level_generator.rs
@@ -1,6 +1,7 @@
 use tile_engine::{TileEngine, TileRect};
 use rand::{SeedableRng, StdRng, Rng};
 use std::collections::VecDeque;
+use std::cmp::{min, max};
 
 pub type Room = [u32; 4];
 
@@ -26,6 +27,7 @@ struct SubDungeon {
     pub deep: u32,
     pub childs: Option<[usize; 2]>,
     pub parent: Option<usize>,
+    pub bounding_rect: Option<Room>,
 }
 
 impl SubDungeon {
@@ -38,6 +40,7 @@ impl SubDungeon {
             deep: deep,
             childs: None,
             parent: parent,
+            bounding_rect: None,
         }
     }
 }
@@ -91,42 +94,76 @@ pub fn generate_level(tile_engine: &mut TileEngine, seed: &[usize], w: u32, h: u
             queue.push_back(dungeon.len()-1);
         }
     }
+    // generation rooms
     let mut rooms = Vec::<Room>::new();
     queue.clear();
-    for d in dungeon.iter() {
-        if d.childs.is_none() {
-            tile_engine.add_tile(d.x as f64, d.y as f64, d.width as i32, d.height as i32, 1);
-            // generation rooms
-            let room_w = rng.gen_range(d.width/2, 4*d.width/5 - d.width/20);
-            let room_h = rng.gen_range(d.height/2, 4*d.height/5 - d.height/20);
-            let room_x = rng.gen_range(d.x + d.width/20, d.x + d.width - room_w - d.width/20);
-            let room_y = rng.gen_range(d.y + d.height/20, d.y + d.height - room_h - d.height/20);
-            tile_engine.add_tile(room_x as f64, room_y as f64, room_w as i32, room_h as i32, 2);
-            rooms.push([room_x, room_y, room_w, room_h]);
+    for i in 0..dungeon.len() {
+        //let has_childs = dungeon[i].childs.is_some();
+        if dungeon[i].childs.is_none() {
+            let parent = {
+                let d = dungeon[i];
+                tile_engine.add_tile(d.x as f64, d.y as f64, d.width as i32, d.height as i32, 1);
+                // generation rooms
+                let mut room = [0,0,0,0];
+                room[2] = rng.gen_range(d.width/2, 4*d.width/5 - d.width/20);
+                room[3] = rng.gen_range(d.height/2, 4*d.height/5 - d.height/20);
+                room[0] = rng.gen_range(d.x + d.width/20, d.x + d.width - room[2] - d.width/20);
+                room[1] = rng.gen_range(d.y + d.height/20, d.y + d.height - room[3] - d.height/20);
+                tile_engine.add_tile(room[0] as f64, room[1] as f64, room[2] as i32, room[3] as i32, 2);
+                rooms.push(room);
+                dungeon[i].bounding_rect = Some(room.clone());
+                dungeon[i].parent.unwrap()
+            };
             // push parent to generate corridor
-            queue.push_back(d.parent.unwrap());
+            let childs = dungeon[parent].childs.unwrap();
+            //println!("{:?}", dungeon[childs[0]]);
+            //println!("{:?}", dungeon[childs[1]]);
+            if (dungeon[childs[0]].bounding_rect.is_some()) && (dungeon[childs[1]].bounding_rect.is_some()) {
+                queue.push_back(parent);
+            }
         }
     }
+    // generating coridors
     while !queue.is_empty() {
-        let p = dungeon[queue.pop_front().unwrap()];
-        if let Some(pp) = p.parent {
-            queue.push_back(pp);
+        let i = queue.pop_front().unwrap();
+        let p = dungeon[i];
+        //println!("{:?}", p);
+        let childs = p.childs.unwrap();
+        let (ch1, ch2) = (dungeon[childs[0]], dungeon[childs[1]]);
+        //println!("{:?}", ch1);
+        //println!("{:?}", ch2);
+        let (bx1, bx2) = (ch1.bounding_rect.unwrap(), ch2.bounding_rect.unwrap());
+        let mut coridor = [0,0,0,0];
+        if ch1.x != ch2.x { // horisontal
+            let max_min_y = max(bx1[1], bx2[1]);
+            let min_max_y = min(bx1[1]+bx1[3], bx2[1]+bx2[3]);
+            coridor[0] = bx1[0] + bx1[2];
+            coridor[1] = rng.gen_range(max_min_y, min_max_y-15);
+            coridor[2] = bx2[0] - bx1[0] - bx1[2];
+            coridor[3] = 15;
         }
-        let (ch1, ch2) = (dungeon[p.childs.unwrap()[0]], dungeon[p.childs.unwrap()[1]]);
-        let mut room = [0,0,0,0];
-        if ch1.x != ch2.x {
-            room[0] = ch1.x + ch1.width/2;
-            room[1] = ((ch1.y*2 + ch1.height)/2 + (ch2.y*2 + ch2.height)/2)/2;
-            room[2] = ch2.x - ch1.x + ch2.width/2 - ch1.width/2;
-            room[3] = 10;
+        else { //vertical
+            let max_min_x = max(bx1[0], bx2[0]);
+            let min_max_x = min(bx1[0]+bx1[2], bx2[0]+bx2[2]);
+            coridor[0] = rng.gen_range(max_min_x, min_max_x-15);
+            coridor[1] = bx1[1] + bx1[3];
+            coridor[2] = 15;
+            coridor[3] = bx2[1] - bx1[1] - bx1[3];
         }
-        else {
-            room[0] = ((ch1.x*2 + ch1.width)/2 + (ch2.x*2 + ch2.width)/2)/2;
-            room[1] = ch1.y + ch1.height/2;
-            room[2] = 10;
-            room[3] = ch2.y - ch1.y + ch2.height/2 - ch1.height/2;
+        tile_engine.add_tile(coridor[0] as f64, coridor[1] as f64, coridor[2] as i32, coridor[3] as i32, 3);
+        // generation bounding box for parent subdungeon
+        let min_x = min(bx1[0], bx2[0]);
+        let max_x = max(bx1[0]+bx1[2], bx2[0]+bx2[2]);
+        let min_y = min(bx1[1], bx2[1]);
+        let max_y = max(bx1[1]+bx1[3], bx2[1]+bx2[3]);
+        dungeon[i].bounding_rect = Some([min_x, min_y, max_x-min_x, max_y-min_y]);
+        // if both childs have bouinding rect => add parent to queue
+        if let Some(pp) = dungeon[i].parent {
+            let childs = dungeon[pp].childs.unwrap();
+            if (dungeon[childs[0]].bounding_rect.is_some()) && (dungeon[childs[1]].bounding_rect.is_some()) {
+                queue.push_back(pp);
+            }
         }
-        tile_engine.add_tile(room[0] as f64, room[1] as f64, room[2] as i32, room[3] as i32, 3);
     }
     //print!("{:?}", dungeon);
     //print!("{:?}", rooms);
