54f75d03236f4d3edf93eb9f372648cf9034b965
Alexander <alexander@kuvaev.me>
2016-02-28 02:23:55 +0300

    rewrite of coridors generation algorithm

--- src/level_generator.rs
+++ src/level_generator.rs
@@ -17,6 +17,8 @@ const DN_SPLIT_COEFF: u32 = 70;
 const DN_DEFAULT_CHANCE: u32 = 100;
 // Min size splittable
 const DN_MIN_SP_SIZE: u32 = 150;
+// Coridor width
+const DN_CORIDOR_WIDTH: u32 = 15;
 
 #[derive(Default, Clone, Copy, Debug)]
 struct SubDungeon {
@@ -43,6 +45,62 @@ impl SubDungeon {
             bounding_rect: None,
         }
     }
+}
+
+fn generate_coridor_vert(tile_engine: &mut TileEngine, bx1: Room, bx2: Room, rng: &mut StdRng) {
+    let max_min_x = max(bx1[0], bx2[0]);
+    let min_max_x = min(bx1[0]+bx1[2], bx2[0]+bx2[2])-DN_CORIDOR_WIDTH;
+    let mut coridor = [0,0,DN_CORIDOR_WIDTH,0];
+    coridor[1] = bx1[1] + bx1[3];
+    coridor[3] = bx2[1] - bx1[1] - bx1[3];
+    if max_min_x < min_max_x {
+        // I like coridor
+        coridor[0] = rng.gen_range(max_min_x, min_max_x);
+    }
+    else {
+        // L like coridor
+        coridor[0] = rng.gen_range(bx1[0], bx1[0]+bx1[2]);
+        coridor[3] += bx2[3]/2;
+        let mut h_coridor = [0,coridor[1]+coridor[3]-DN_CORIDOR_WIDTH,0,DN_CORIDOR_WIDTH];
+        if max_min_x == bx1[0] {
+            h_coridor[0] = min_max_x;
+            h_coridor[2] = coridor[0] - h_coridor[0];
+        }
+        else {
+            h_coridor[0] = coridor[0];
+            h_coridor[2] = max_min_x - coridor[0];
+        }
+        tile_engine.add_tile(h_coridor[0] as f64, h_coridor[1] as f64, h_coridor[2] as i32, h_coridor[3] as i32, 3);
+    }
+    tile_engine.add_tile(coridor[0] as f64, coridor[1] as f64, coridor[2] as i32, coridor[3] as i32, 3);
+}
+
+fn generate_coridor_hor(tile_engine: &mut TileEngine, bx1: Room, bx2: Room, rng: &mut StdRng) {
+    let max_min_y = max(bx1[1], bx2[1]);
+    let min_max_y = min(bx1[1]+bx1[3], bx2[1]+bx2[3])-DN_CORIDOR_WIDTH;
+    let mut coridor = [0,0,0,DN_CORIDOR_WIDTH];
+    coridor[0] = bx1[0] + bx1[2];
+    coridor[2] = bx2[0] - bx1[0] - bx1[2];
+    if max_min_y < min_max_y {
+        // - like coridor
+        coridor[1] = rng.gen_range(max_min_y, min_max_y);
+    }
+    else {
+        // L like coridor
+        coridor[1] = rng.gen_range(bx1[1], bx1[1]+bx1[3]);
+        coridor[2] += bx2[2]/2;
+        let mut v_coridor = [coridor[0]+coridor[2]-DN_CORIDOR_WIDTH,0,DN_CORIDOR_WIDTH,0];
+        if max_min_y == bx1[1] {
+            v_coridor[1] = min_max_y;
+            v_coridor[3] = coridor[1] - v_coridor[1];
+        }
+        else {
+            v_coridor[1] = coridor[1];
+            v_coridor[3] = max_min_y - coridor[1];
+        }
+        tile_engine.add_tile(v_coridor[0] as f64, v_coridor[1] as f64, v_coridor[2] as i32, v_coridor[3] as i32, 3);
+    }
+    tile_engine.add_tile(coridor[0] as f64, coridor[1] as f64, coridor[2] as i32, coridor[3] as i32, 3);
 }
 
 pub fn generate_level(tile_engine: &mut TileEngine, seed: &[usize], w: u32, h: u32) -> Vec<Room> {
@@ -79,7 +137,7 @@ pub fn generate_level(tile_engine: &mut TileEngine, seed: &[usize], w: u32, h: u
             let mut w1 = w;
             let mut h1 = h;
             // Split vertical or not
-            if (w > DN_MIN_SP_SIZE) && (rng.gen_range(0, 2) == 1) {
+            if (h < DN_MIN_SP_SIZE) || ((w > DN_MIN_SP_SIZE) && (rng.gen_range(0, 2) == 1)) {
                 x2 = x1 + w*coef/100;
                 w1 = x2 - x1;
             }
@@ -105,8 +163,8 @@ pub fn generate_level(tile_engine: &mut TileEngine, seed: &[usize], w: u32, h: u
                 tile_engine.add_tile(d.x as f64, d.y as f64, d.width as i32, d.height as i32, 1);
                 // generation rooms
                 let mut room = [0,0,0,0];
-                room[2] = rng.gen_range(d.width/2, 4*d.width/5 - d.width/20);
-                room[3] = rng.gen_range(d.height/2, 4*d.height/5 - d.height/20);
+                room[2] = rng.gen_range(15*d.width/20, 18*d.width/20);
+                room[3] = rng.gen_range(15*d.height/20, 18*d.height/20);
                 room[0] = rng.gen_range(d.x + d.width/20, d.x + d.width - room[2] - d.width/20);
                 room[1] = rng.gen_range(d.y + d.height/20, d.y + d.height - room[3] - d.height/20);
                 tile_engine.add_tile(room[0] as f64, room[1] as f64, room[2] as i32, room[3] as i32, 2);
@@ -133,30 +191,21 @@ pub fn generate_level(tile_engine: &mut TileEngine, seed: &[usize], w: u32, h: u
         //println!("{:?}", ch1);
         //println!("{:?}", ch2);
         let (bx1, bx2) = (ch1.bounding_rect.unwrap(), ch2.bounding_rect.unwrap());
-        let mut coridor = [0,0,0,0];
-        if ch1.x != ch2.x { // horisontal
-            let max_min_y = max(bx1[1], bx2[1]);
-            let min_max_y = min(bx1[1]+bx1[3], bx2[1]+bx2[3]);
-            coridor[0] = bx1[0] + bx1[2];
-            coridor[1] = rng.gen_range(max_min_y, min_max_y-15);
-            coridor[2] = bx2[0] - bx1[0] - bx1[2];
-            coridor[3] = 15;
+        if ch1.x == ch2.x {
+            generate_coridor_vert(tile_engine, bx1, bx2, &mut rng);
         }
-        else { //vertical
-            let max_min_x = max(bx1[0], bx2[0]);
-            let min_max_x = min(bx1[0]+bx1[2], bx2[0]+bx2[2]);
-            coridor[0] = rng.gen_range(max_min_x, min_max_x-15);
-            coridor[1] = bx1[1] + bx1[3];
-            coridor[2] = 15;
-            coridor[3] = bx2[1] - bx1[1] - bx1[3];
+        else {
+            generate_coridor_hor(tile_engine, bx1, bx2, &mut rng);
         }
-        tile_engine.add_tile(coridor[0] as f64, coridor[1] as f64, coridor[2] as i32, coridor[3] as i32, 3);
         // generation bounding box for parent subdungeon
-        let min_x = min(bx1[0], bx2[0]);
-        let max_x = max(bx1[0]+bx1[2], bx2[0]+bx2[2]);
-        let min_y = min(bx1[1], bx2[1]);
-        let max_y = max(bx1[1]+bx1[3], bx2[1]+bx2[3]);
-        dungeon[i].bounding_rect = Some([min_x, min_y, max_x-min_x, max_y-min_y]);
+        dungeon[i].bounding_rect = {
+            if rng.gen_range(0, 2) == 1 {
+                Some(bx1.clone())
+            }
+            else {
+                Some(bx2.clone())
+            }
+        };
         // if both childs have bouinding rect => add parent to queue
         if let Some(pp) = dungeon[i].parent {
             let childs = dungeon[pp].childs.unwrap();
